7856df9dbe0ab9a5faf98028a27b178a5f1e700a
suno0140 <vlfxhd15@naver.com>
2023-04-11 16:24:10 +0900

    design: css변경 tailwind로 적용

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
     let newTodo = {
       id: Date.now(),
       title: value,
-      complete: false,
+      completed: false,
     };
 
     setTodoData((prev) => [...prev, newTodo]);
@@ -21,12 +21,11 @@ export default function App() {
   };
 
   return (
-    <div className="container">
-      <div className="todoBlock">
-        <div className="title">
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow md:max-w-lg lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
         </div>
-
         <List todoData={todoData} setTodoData={setTodoData} />
 
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
--- src/components/Form.js
+++ src/components/Form.js
@@ -7,20 +7,20 @@ export default function Form({ handleSubmit, value, setValue }) {
 
   return (
     <div>
-      <form style={{ display: "flex" }}>
+      <form className="flex pt-2">
         <input
+          className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
           type="text"
           name="value"
-          style={{ flex: "10", padding: "5px" }}
           placeholder="해야 할 일을 입력하세요."
           value={value}
           onChange={handleChange}
         />
         <input
+          className="border p-2 text-blue-400 border-blue-400 rounded hover:text-white hover:bg-blue-200"
           type="submit"
           value="입력"
           classname="btn"
-          style={{ flex: "1" }}
           onClick={handleSubmit}
         />
       </form>
