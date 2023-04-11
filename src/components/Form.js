77c11b91b8c604918446c729d440a28aa0bf08d4
suno0140 <vlfxhd15@naver.com>
2023-04-11 14:44:50 +0900

    refactor: Form 컴포넌트 분리

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Form({ handleSubmit, value, setValue }) {
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <form style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="value"
+          style={{ flex: "10", padding: "5px" }}
+          placeholder="해야 할 일을 입력하세요."
+          value={value}
+          onChange={handleChange}
+        />
+        <input
+          type="submit"
+          value="입력"
+          classname="btn"
+          style={{ flex: "1" }}
+          onClick={handleSubmit}
+        />
+      </form>
+    </div>
+  );
+}
