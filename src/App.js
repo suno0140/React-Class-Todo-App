72763752fc105273d7d4fdb5f8b85f9c54d36db1
suno0140 <vlfxhd15@naver.com>
2023-04-12 16:17:29 +0900

    feat: 모두 지우기 버튼 생성

--- src/App.js
+++ src/App.js
@@ -19,12 +19,16 @@ export default function App() {
     setTodoData((prev) => [...prev, newTodo]);
     setValue("");
   };
+  const handleRemoveAll = () => {
+    setTodoData([]);
+  };
 
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow md:max-w-lg lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveAll}>모두 지우기</button>
         </div>
         <Lists todoData={todoData} setTodoData={setTodoData} />
 
